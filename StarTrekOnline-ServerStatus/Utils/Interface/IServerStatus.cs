b9708848aecfc361f4f222da57d375e177d16734
辉夜 <rx700@vip.qq.com>
2024-01-05 10:47:35 +0800

    Fixed maintenance time issue.

--- StarTrekOnline-ServerStatus/Utils/Interface/IServerStatus.cs
+++ StarTrekOnline-ServerStatus/Utils/Interface/IServerStatus.cs
@@ -65,8 +65,7 @@ namespace StarTrekOnline_ServerStatus
         {
             if (debug)
             {
-                return
-                    "{\"result\":\"success\",\"news\":[{\"newsID\":11555873,\"title\":\"PC Patch Notes for 10\\/5\\/23\",\"postDate\":1696489200,\"url\":\"https:\\/\\/www.arcgames.com\\/en\\/games\\/star-trek-online\\/news\\/detail\\/11555873\",\"date\":\"2023-10-05\"},{\"newsID\":11555803,\"title\":\"Command the Dauntless II!\",\"postDate\":1696431600,\"url\":\"https:\\/\\/www.arcgames.com\\/en\\/games\\/star-trek-online\\/news\\/detail\\/11555803\",\"date\":\"2023-10-04\"}],\"banners\":[{\"newsID\":\"11555803\",\"imageLoc\":\"https:\\/\\/pwimages-a.akamaihd.net\\/arc\\/60\\/05\\/6005b6427b307db75446f74743f4d0db1696377153.png\",\"clientImageLoc\":\"\",\"schedule\":\"true\"},{\"newsID\":\"11554993\",\"imageLoc\":\"https:\\/\\/pwimages-a.akamaihd.net\\/arc\\/f4\\/39\\/f4397cd9f15fed16bafd3e87db0a70731695874491.png\",\"clientImageLoc\":\"\",\"schedule\":\"true\"},{\"newsID\":\"11553903\",\"imageLoc\":\"https:\\/\\/pwimages-a.akamaihd.net\\/arc\\/cb\\/28\\/cb2857f30572a5783fe56f0acfb51e1b1694533798.png\",\"clientImageLoc\":\"\",\"schedule\":\"true\"},{\"newsID\":\"11553753\",\"imageLoc\":\"https:\\/\\/pwimages-a.akamaihd.net\\/arc\\/21\\/c3\\/21c3dcc75dc481a9c953dae4c090541b1694268654.png\",\"clientImageLoc\":\"\",\"schedule\":\"true\"},{\"newsID\":\"11553353\",\"imageLoc\":\"https:\\/\\/pwimages-a.akamaihd.net\\/arc\\/9f\\/c6\\/9fc64701663148ae2956da9f7b21a9a41694543132.png\",\"clientImageLoc\":\"\",\"schedule\":\"true\"}],\"left_ad\":{\"newsID\":\"11552853\",\"imageLoc\":\"https:\\/\\/pwimages-a.akamaihd.net\\/arc\\/a4\\/a9\\/a4a9e3209fdc7e3b1effe2174b9ced251693280101.png\",\"clientImageLoc\":\"\",\"schedule\":\"true\"},\"message\":\"We will be performing shard maintenance on December 1 from 7-9:30 AM Pacific (08:53-09:00 UTC). Please check the forums.\",\"baseurl\":\"https:\\/\\/www.arcgames.com\\/en\\/games\\/star-trek-online\\/news\\/detail\\/\",\"server_status\":\"down\"}";
+                return "{\"result\":\"success\",\"news\":[{\"newsID\":11555873,\"title\":\"PC Patch Notes for 10\\/5\\/23\",\"postDate\":1696489200,\"url\":\"https:\\/\\/www.arcgames.com\\/en\\/games\\/star-trek-online\\/news\\/detail\\/11555873\",\"date\":\"2023-10-05\"},{\"newsID\":11555803,\"title\":\"Command the Dauntless II!\",\"postDate\":1696431600,\"url\":\"https:\\/\\/www.arcgames.com\\/en\\/games\\/star-trek-online\\/news\\/detail\\/11555803\",\"date\":\"2023-10-04\"}],\"banners\":[{\"newsID\":\"11555803\",\"imageLoc\":\"https:\\/\\/pwimages-a.akamaihd.net\\/arc\\/60\\/05\\/6005b6427b307db75446f74743f4d0db1696377153.png\",\"clientImageLoc\":\"\",\"schedule\":\"true\"},{\"newsID\":\"11554993\",\"imageLoc\":\"https:\\/\\/pwimages-a.akamaihd.net\\/arc\\/f4\\/39\\/f4397cd9f15fed16bafd3e87db0a70731695874491.png\",\"clientImageLoc\":\"\",\"schedule\":\"true\"},{\"newsID\":\"11553903\",\"imageLoc\":\"https:\\/\\/pwimages-a.akamaihd.net\\/arc\\/cb\\/28\\/cb2857f30572a5783fe56f0acfb51e1b1694533798.png\",\"clientImageLoc\":\"\",\"schedule\":\"true\"},{\"newsID\":\"11553753\",\"imageLoc\":\"https:\\/\\/pwimages-a.akamaihd.net\\/arc\\/21\\/c3\\/21c3dcc75dc481a9c953dae4c090541b1694268654.png\",\"clientImageLoc\":\"\",\"schedule\":\"true\"},{\"newsID\":\"11553353\",\"imageLoc\":\"https:\\/\\/pwimages-a.akamaihd.net\\/arc\\/9f\\/c6\\/9fc64701663148ae2956da9f7b21a9a41694543132.png\",\"clientImageLoc\":\"\",\"schedule\":\"true\"}],\"left_ad\":{\"newsID\":\"11552853\",\"imageLoc\":\"https:\\/\\/pwimages-a.akamaihd.net\\/arc\\/a4\\/a9\\/a4a9e3209fdc7e3b1effe2174b9ced251693280101.png\",\"clientImageLoc\":\"\",\"schedule\":\"true\"},\"message\":\"We will be performing shard maintenance on January 5 from 7-9:30 AM Pacific (02:34-09:00 UTC). Please check the forums.\",\"baseurl\":\"https:\\/\\/www.arcgames.com\\/en\\/games\\/star-trek-online\\/news\\/detail\\/\",\"server_status\":\"down\"}";
             }
             else
             {
@@ -108,21 +107,12 @@ namespace StarTrekOnline_ServerStatus
 
         private async Task<(DateTime, DateTime)> TimeUntilEvent((string, string, string) times)
         {
-            string monthDay = $"{times.Item1} {times.Item2}";
             string[] timeRange = times.Item3.Split('-');
-
-            DateTime currentDate = DateTime.Now;
-            int currentYear = currentDate.Year;
-
-            DateTime monthDayDate = DateTime.ParseExact(monthDay, "MMMM d", CultureInfo.InvariantCulture);
 
             DateTime startTime = DateTime.ParseExact(timeRange[0], "H:mm", CultureInfo.InvariantCulture);
             DateTime endTime = DateTime.ParseExact(timeRange[1], "H:mm", CultureInfo.InvariantCulture);
-
-            DateTime startEventTime = monthDayDate.Date.AddHours(startTime.Hour).AddMinutes(startTime.Minute).AddHours(8);
-            DateTime endEventTime = monthDayDate.Date.AddHours(endTime.Hour).AddMinutes(endTime.Minute).AddHours(8);
             
-            return (startEventTime, endEventTime);
+            return (startTime, endTime);
         }
 
         private (int, int, int, int) FormatTimeDifference(TimeSpan timeDifference)
